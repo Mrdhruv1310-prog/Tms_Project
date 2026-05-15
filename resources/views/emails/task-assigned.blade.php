7e84ebfabc02328a4f0b4e20e11a86ef50d24b24
Dhruv M Panchal <panchalmrdhruv@gmail.com>
2026-05-15 13:29:18 +0530

    When Assign task they mail throgh go in task link

--- resources/views/emails/task-assigned.blade.php
+++ resources/views/emails/task-assigned.blade.php
@@ -122,7 +122,7 @@
 
                 <!-- Button -->
                 <div class="btn" style="text-align:center;">
-                    <a href="{{ url('/tasks/' . $task->id) }}"
+                    <a href="{{ url('/tasks?task_view=tasks') }}"
                         style="background-color:#ff8300; color:#ffffff;padding:12px 25px;text-decoration:none;
                     border-radius:5px;display:inline-block;font-weight:bold;">
                         View Task
