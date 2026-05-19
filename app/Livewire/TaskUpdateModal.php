cfdce0f1b0596a48000ef6a19707406dae4b4aa3
Dhruv M Panchal <panchalmrdhruv@gmail.com>
2026-05-19 17:10:48 +0530

    Some Final Changes

--- app/Livewire/TaskUpdateModal.php
+++ app/Livewire/TaskUpdateModal.php
@@ -78,25 +78,11 @@ class TaskUpdateModal extends Component
             }
             $taskStatus = 'pending';
 
-            if (in_array('in_progress', $statuses)) {
-                dd('1',$statuses);
-                dd('1',$taskStatus);
+            if (in_array('pending', $statuses)) {
                 $taskStatus = 'in_progress';
-            } elseif (
-                count($statuses) > 0 &&
-                count(array_unique($statuses)) === 1 &&
-                in_array('complete_intimation', $statuses)
-            ) {
-                dd('2',$statuses);
-                dd('2',$taskStatus);
+            } elseif (in_array('in_progress', $statuses)) {
                 $taskStatus = 'completed';
-            } elseif (in_array('complete_intimation', $statuses) && !in_array('in_progress', $statuses)) {
-                dd('3',$statuses);
-                dd('3',$taskStatus);
-                $taskStatus = 'pending';
             } else {
-                dd('4',$statuses);
-                dd('4',$taskStatus);
                 $taskStatus = 'pending';
             }
 
