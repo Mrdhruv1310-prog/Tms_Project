06c872f375c797e3087d7715fb281406c0799b94
Dhruv M Panchal <panchalmrdhruv@gmail.com>
2026-05-19 15:09:37 +0530

    Find Error 3

--- app/Livewire/TaskUpdateModal.php
+++ app/Livewire/TaskUpdateModal.php
@@ -34,7 +34,6 @@ class TaskUpdateModal extends Component
 
     public function updateTaskRemark(User $user)
     {
-        dd($this->remark, $this->status, $this->task);
         $this->validate([
             'remark' => 'required|string|max:255',
         ]);
